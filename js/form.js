55170c237ba8b065eadc898a9ffc7cf4231a26e0
Lareta <lareta064@gmail.com>
2018-07-21 17:57:53 +0300

    carousel

---
+++ js/form.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+
+  /*$('.phone').mask("+7(999)999-9999", {autoclear: false});
+
+  $.validator.addMethod("checkMask", function(value, element) {
+       return this.optional(element) || /\+\d{1}\(\d{3}\)\d{3}-\d{4}/g.test(value); 
+  }, "Введенный номер не корректен");*/
+
+// jQuery Validate JS
+  $(".contact-form").validate({
+    rules: {
+      name: { required: true },
+      email: { required: true, email: true },
+      // skype:  { required: true },
+      /*phone:{
+        required: true,
+        'checkMask': true
+      },*/
+      
+      message: { required: true }
+    },
+
+    messages: {
+      phone: {
+        required: "Пожалуйста, введите номер телефона",
+      }
+     // name: "Пожалуйста, введите свое имя",
+      //email: {
+       // required: "Пожалуйста, введите свой email",
+       //email: "Email адрес должен быть в формате name@domain.com . Возможно вы ввели email с ошибкой."
+    },
+
+    submitHandler: function(form) {
+      ajaxFormSubmit();
+    }
+
+  })
+
+  
+   // закрываем окно, нажимая на крестик
+   $('#form-close').click(function() {
+     $(".overlay_popup").hide();
+      $("#popup1").hide();
+   });
+      
+
+})
+
+    
--- js/main.js
+++ js/main.js
@@ -4,6 +4,27 @@ $(function() {
   $(window).resize(function(){
     $('body').css('font-size',(6+0.5*$('body').width()/100)+'px');
   });
+
+	//плавная прокрутка
+    $("a,nav a,a[href='#'],a[rel='m_PageScroll2id'],a.PageScroll2id, a.mouse_scroll").mPageScroll2id({
+      highlightSelector:"nav a"
+    });
+	// end плавная прокрутка
+
+    //animated
+	 wow = new WOW( {
+	     boxClass:     'wow',      // default
+	    animateClass: 'animated', // default
+	    mobile:       false,       // default
+	    offset:       100,          // через сколько пикселей сработает
+	    live:         true        // для мобильных оставить или убрать анимацию
+	    }
+	  )
+	  wow.init();
+	   new WOW().init();
+	   // end animated
+
+
 });
 //parallax
 var scene = document.getElementById('scene');
