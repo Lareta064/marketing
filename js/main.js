51b5f7a4e98a524bac977283db22824ce5b63b56
Lareta <lareta064@gmail.com>
2018-08-04 19:03:26 +0300

    commit 4 august

--- js/main.js
+++ js/main.js
@@ -1,14 +1,19 @@
 /* адаптивный шрифт*/
 $(function() {
+	
   $('body').css('font-size',(6+0.5*$('body').width()/100)+'px');
   $(window).resize(function(){
     $('body').css('font-size',(6+0.5*$('body').width()/100)+'px');
   });
 
 	//плавная прокрутка
-    $("a,nav a,a[href='#'],a[rel='m_PageScroll2id'],a.PageScroll2id, a.mouse_scroll").mPageScroll2id({
-      highlightSelector:"nav a"
-    });
+    //$("a,nav a,a[href='#'],a[rel='m_PageScroll2id'],a.PageScroll2id, a.mouse_scroll").mPageScroll2id({
+    // highlightSelector:"nav a"
+    //});
+
+    $("nav a,a[rel='m_PageScroll2id'],a.PageScroll2id, a.mouse_scroll").mPageScroll2id({
+    	highlightSelector:"nav a"
+     });
 	// end плавная прокрутка
 
 	// смена фона в блоке с формой
@@ -21,7 +26,7 @@ $(function() {
 	     boxClass:     'wow',      // default
 	    animateClass: 'animated', // default
 	    mobile:       true,       // default
-	    offset:       150,          // через сколько пикселей сработает
+	    offset:       110,          // через сколько пикселей сработает
 	    live:         true        // для мобильных оставить или убрать анимацию
 	    }
 	  )
@@ -29,8 +34,69 @@ $(function() {
 	   new WOW().init();
 	   // end animated
 
+		// open modal form
+	$('#open_modal').on('click', function(){
+		$('#overlay').fadeIn('slow');
+		$('#modalForm').css('display','block');
+		
+	});
 
-});
+	$('#modalBtn').on('click', function(){
+		$('#overlay').fadeOut('slow');
+		
+	});
+
+	$('#overlay').on('click', function(){
+		$(this).fadeOut('slow');
+		$('#modalForm').css('display','none');
+	});
+
+	// end open modal form
+
+		// open modal form
+	$('.open_modal-service').on('click', function(){
+		$('#overlay2').fadeIn('slow');
+		$('#modalForm2').css('display','block');
+		
+	});
+
+	$('#modalBtn2').on('click', function(){
+		$('#overlay2').fadeOut('slow');
+		
+	});
+
+	$('#overlay2').on('click', function(){
+		$(this).fadeOut('slow');
+		$('#modalForm2').css('display','none');
+	});
+
+	// end open modal form
+
+
+	//кнопка вверх
+    // hide #back-top first
+    $("#back-top").hide();
+
+    // fade in #back-top
+    $(function () {
+        $(window).scroll(function () { if ($(this).scrollTop() > 200) {
+                    $('#back-top').fadeIn();
+            } else {
+                    $('#back-top').fadeOut();
+            }
+    });
+
+    // scroll body to 0px on click
+    $('#back-top a').click(function () {
+            $('body,html').animate({
+                    scrollTop: 0
+            }, 600); return false;
+    });
+    });
+
+}); 
+
+
 //parallax
 var scene = document.getElementById('scene');
 var parallax = new Parallax(scene);
